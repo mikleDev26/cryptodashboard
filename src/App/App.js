1670c40098316ec140d9caaa3587d87c023dc9b1
Michale Chigorin <mmchigorin@gmail.com>
2020-02-13 23:15:11 +0300

    styled appBar component

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./App.css";
+// import styled, { css } from "styled-components";
+import AppLayout from "./AppLayout";
+import AppBar from "../components/AppBar/AppBar";
 
 function App() {
-  return <div className="App">Main page</div>;
+  return (
+    <AppLayout>
+      <AppBar></AppBar>
+      <div className="App">
+        <h2>Main page</h2>
+      </div>
+    </AppLayout>
+  );
 }
 
 export default App;
