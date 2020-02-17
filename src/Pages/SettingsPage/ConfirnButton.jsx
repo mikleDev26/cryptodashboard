9084b1059e2a8666eb8e3e5559464d102ed5801b
MikleDev26 <justamaizingemail@gmail.com>
2020-02-17 22:02:10 +0300

    create coinGrid

--- src/Pages/SettingsPage/ConfirnButton.jsx
+++ src/Pages/SettingsPage/ConfirnButton.jsx
@@ -1,11 +1,17 @@
 import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { DashBoardContext } from '../../Context/DashBoardProvider';
+import { fontSize1, greenBoxShadow, color3 } from '../../Shared/Styles';
 
 const ConfirmButton = styled.div`
   margin: 20px;
-  color: green;
+  color: ${color3};
+  ${fontSize1}
+  padding: 5px;
   cursor: pointer;
+  &:hover {
+    ${greenBoxShadow}
+  }
 `;
 
 const CenterDiv = styled.div`
